d8c567e62b6e1a901ba96c226262fccf25c47f80
Felix Appiah-Kubi <feddybills17@outlook.com>
2025-09-21 10:47:24 +0100

    work in progress

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import math
 course = "python Programming"
 print(course.upper())
 print(course)
@@ -5,3 +6,29 @@ print(course.strip())
 print(course.replace("p", "j"))
 print("pro" in course)
 print("Pro" in course)
+
+
+#working with numbers 
+print(10 + 3)
+print(10 - 3)
+
+
+print(round(2.9))
+print (abs(-2.9))
+
+print(math.ceil(2.2))
+
+
+
+# trying
+fruit = "Apple"
+print(fruit[1:-1])
+
+print(bool("false"))
+
+# compariosn Operations 
+tempreature = 35
+if tempreature > 30:
+    print("It's warm")
+    print("Drink water")
+print("Done")
