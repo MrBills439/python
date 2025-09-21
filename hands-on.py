2757ab217fe7b24be571b2ad51a742d8130cccfa
Felix Appiah-Kubi <feddybills17@outlook.com>
2025-09-21 15:16:45 +0100

    working on hands on

---
+++ hands-on.py
@@ -0,0 +1,9 @@
+#WELCOME NOTES
+name = input("what is your name ?...")
+age = input("what is your age ?...")
+purpose = input("what is your purpose for this comming?...")
+
+print("\n---------")
+print(f"Wellcome to the family, {name}")
+print(f"You are, {age} old ")
+print(f"My purpose here is {purpose}")
