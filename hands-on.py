2e29947a1ce13a37b4328a88904fd5c03d5341e8
Felix Appiah-Kubi <feddybills17@outlook.com>
2025-09-22 12:28:38 +0100

    working on hands on

--- hands-on.py
+++ hands-on.py
@@ -1,9 +1,34 @@
 #WELCOME NOTES
-name = input("what is your name ?...")
-age = input("what is your age ?...")
-purpose = input("what is your purpose for this comming?...")
+#name = input("what is your name ?...")
+#age = input("what is your age ?...")
+#purpose = input("what is your purpose for this comming?...")
+#
+#print("\n---------")
+#print(f"Wellcome to the family, {name}")
+#print(f"You are, {age} old ")
+#print(f"My purpose here is {purpose}")
 
-print("\n---------")
-print(f"Wellcome to the family, {name}")
-print(f"You are, {age} old ")
-print(f"My purpose here is {purpose}")
+ 
+
+
+pizza = 2.99
+coke = 0.99
+
+total = pizza + coke
+
+tip = total * 0.3
+print(tip)
+
+#
+#Create a temperature.py program that converts a number from Fahrenheit (°F) to Celsius (°C).
+#
+#Google the current temperature of Brooklyn, NY (where Codédex is based) in °F.
+#
+#Use the following formula and write it out in Python:
+#
+#
+#Celsius= 
+#1.8
+#(Fahrenheit−32)
+
+
