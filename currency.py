640a3d1a70c37f5700e507aa22f5300bd9e8836d
Felix Appiah-Kubi <feddybills17@outlook.com>
2025-09-29 14:38:33 +0100

    hands on python

---
+++ currency.py
@@ -0,0 +1,17 @@
+pesos = int(input('What do you have left in pesos? '))
+soles = int(input('What do you have left in soles? '))
+reais = int(input('What do you have left in reais? '))
+
+total = pesos * 0.00025 + soles * 0.28 + reais * 0.21
+
+print(total)
+
+butterflies = 10
+beetles = 12
+ladybugs = 20
+
+print('I caught ' + butterflies + ' 🦋 butterflies!')
+print('I caught ' + beetles + ' 🪲 beetles!')
+print('I caught ' + ladybugs + ' 🐞 ladybugs!)
+
+print('I caught ' + str(total) + ' total bugs!')
