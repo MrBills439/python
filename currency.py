5f5db1796e7e004ed6cfa209eb0f0ef25000479e
Felix Appiah-Kubi <feddybills17@outlook.com>
2025-09-29 14:52:16 +0100

    hands on python

--- currency.py
+++ currency.py
@@ -10,8 +10,9 @@ butterflies = 10
 beetles = 12
 ladybugs = 20
 
-print('I caught ' + butterflies + ' 🦋 butterflies!')
-print('I caught ' + beetles + ' 🪲 beetles!')
-print('I caught ' + ladybugs + ' 🐞 ladybugs!)
+total = butterflies + beetles + ladybugs
+print('I caught ' + str(butterflies) + ' 🦋 butterflies!')
+print('I caught ' + str(beetles) + ' 🪲 beetles!')
+print('I caught ' + str(ladybugs) + ' 🐞 ladybugs!')
 
 print('I caught ' + str(total) + ' total bugs!')
