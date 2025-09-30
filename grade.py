ce664e7a21547f850693aa1b779b2e2b954ce52e
Felix Appiah-Kubi <feddybills17@outlook.com>
2025-09-30 11:42:41 +0100

    Heads on pyhton

--- grade.py
+++ grade.py
@@ -6,3 +6,13 @@ else:
     print("You failed")
 
 
+# grade.py 
+# i did this code to handle a decimal number in other for anyone who as decimal number.
+try:
+    grade = float(input("What is your grade? "))
+    if grade > 60:
+        print("You passed")
+    else:
+        print("You failed")
+except ValueError:
+    print("Please enter a valid number.")
