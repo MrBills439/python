c85e723cc78f87f0bd6e80030bb27171d095299e
Felix Appiah-Kubi <feddybills17@outlook.com>
2025-09-30 20:04:23 +0100

    Heads on pyhton

--- sourting.py
+++ sourting.py
@@ -27,3 +27,26 @@ elif answer == 2:
   slytherin = slytherin + 1
 else:
   print('Wrong input.')
+
+
+  # ~~~~~~~~~~~~~~~ Question 2 ~~~~~~~~~~~~~~~
+
+print("\nQ2) When I'm dead, I want people to remember me as:")
+
+print('  1) The Goat')
+print('  2) The Great')
+print('  3) The Wise')
+print('  4) The Bold')
+
+answer = int(input('Enter your answer (1-4): '))
+
+if answer == 1:
+  hufflepuff = hufflepuff + 2
+elif answer == 2:
+  slytherin = slytherin + 2
+elif answer == 3:
+  ravenclaw = ravenclaw + 2
+elif answer == 4:
+  gryffindor = gryffindor + 2
+else:
+  print('Wrong input.')
