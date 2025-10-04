0cb94078fd1c19545ce9fef8c5dc185104af49c9
Felix Appiah-Kubi <feddybills17@outlook.com>
2025-10-04 20:42:37 +0100

    trying my hands on pyhton

--- guess.py
+++ guess.py
@@ -11,4 +11,9 @@ tries = 0
 
 while guess != 6 and tries < 5:
   guess = int(input('Guess the number: '))
-  # update tries
+  tries = tries + 1
+
+if guess != 6:
+  print('You ran out of tries.')
+else:
+  print('You got it!')
