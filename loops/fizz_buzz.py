1ce1e076cb89834792f8dbdd31283310e671eaaa
Felix Appiah-Kubi <feddybills17@outlook.com>
2025-10-06 19:21:45 +0100

    trying my hands on pyhton

---
+++ loops/fizz_buzz.py
@@ -0,0 +1,15 @@
+for i in range(10):
+  print(i)
+
+  # Fizz Buzz 🐝
+# Codédex
+
+for num in range(1, 101):
+  if num % 3 == 0 and num % 5 == 0:
+    print('FizzBuzz')
+  elif num % 3 == 0:
+    print('Fizz')
+  elif num % 5 == 0:
+    print('Buzz')
+  else:
+    print(num)
